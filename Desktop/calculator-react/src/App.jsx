f585a4b2307655ee5df43e030868487415969378
MailBIT Sales <yuvalleberstein21@gmail.com>
2024-07-22 17:18:27 +0300

    calculator final

---
+++ Desktop/calculator-react/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Calculator from './Pages/Calculator'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="container">
+        <div className="calculator">
+
+          <Calculator />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
